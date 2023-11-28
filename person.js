83e8c8caabfee3c0421c59e7f7a01011860be370
Jordan Cox <jordancox747@outlook.com>
2023-11-28 05:58:25 -0700

    mongodb connection working

--- person.js
+++ person.js
@@ -1,2 +1,38 @@
 const mongoose = require("mongoose");
-// schemas next
+
+const Schema = mongoose.Schema;
+
+const addressSchema = new Schema({
+  city: { type: String, required: true },
+  street: { type: String, required: true },
+  apartment: Number
+});
+
+
+const personSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  age: { type: Number, required: true },
+  address: addressSchema
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const david = new Person({
+  firstName: "David",
+  lastName: "Smith",
+  age: 25,
+  address: {
+    city: "New York City",
+    street: "Main St.",
+    apartment: 123
+  }
+});
+
+async function run(person) {
+  await mongoose.connect("mongodb://localhost:27017/peopleDB");
+
+  await person.save();
+}
+
+run(david);
